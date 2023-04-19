e5f2b920f2aaa8b8c36bde272299c731c55b1276
Mikhail <imisha.volkov@gmail.com>
2023-04-19 14:02:59 +0300

    added BooksTable with custom cell

--- OpenLibraryAPI/Controller/MainViewController.swift
+++ OpenLibraryAPI/Controller/MainViewController.swift
@@ -14,13 +14,28 @@ class MainViewController: UIViewController {
 
     // MARK: - Views
     private lazy var searchView = SearchView()
+    private lazy var booksTable: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(BookTableViewCell.self, forCellReuseIdentifier: BookTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
 
+        return tableView
+    }()
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
         setupTopBar()
+        setupViews()
+        setupConstraints()
         searchView.delegate = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupStatusBar()
     }
 
     // MARK: - Functions
@@ -30,13 +45,68 @@ class MainViewController: UIViewController {
         navigationItem.titleView = searchView
     }
 
+    private func setupStatusBar() {
+        if let statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame {
+            let statusBar = UIView(frame: statusBarFrame)
+            statusBar.backgroundColor = navigationController?.navigationBar.backgroundColor
+            view.addSubview(statusBar)
+        }
+    }
+
+    private func setupViews() {
+        view.addSubview(booksTable)
+
+    }
+
+    private func setupConstraints() {
+        var constraints = [NSLayoutConstraint]()
+
+        constraints.append(booksTable.trailingAnchor.constraint(equalTo: view.trailingAnchor))
+        constraints.append(booksTable.topAnchor.constraint(equalTo: view.topAnchor))
+        constraints.append(booksTable.trailingAnchor.constraint(equalTo: view.trailingAnchor))
+        constraints.append(booksTable.bottomAnchor.constraint(equalTo: view.bottomAnchor))
+        constraints.append(booksTable.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
+// MARK: - UITableViewDataSource
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        books.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: BookTableViewCell.identifier,
+                                                       for: indexPath) as? BookTableViewCell else { return UITableViewCell() }
+
+        let image = dataFetcher.getImageUrl(forCoverId: books[indexPath.row].coverI, size: .small)
+        cell.setupCell(doc: books[indexPath.row], image: image)
+
+        return cell
+    }
+
+
+}
+
+// MARK: - UITableViewDelegate
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        90
+    }
+}
 // MARK: - SearchViewDelegate
 extension MainViewController: SearchViewDelegate {
     func findInfo(forText text: String) {
         dataFetcher.getSearchingResult(forRequest: text) { [weak self] response in
-            print(response.docs)
+            guard let self = self else { return }
+            self.books = response.docs
+            self.booksTable.reloadData()
         }
     }
 }
---
+++ OpenLibraryAPI/View/BookTableViewCell.swift
@@ -0,0 +1,101 @@
+//
+//  BookTableViewCell.swift
+//  OpenLibraryAPI
+//
+//  Created by Misha Volkov on 19.04.23.
+//
+
+import UIKit
+
+final class BookTableViewCell: UITableViewCell {
+    // MARK: - Constants/Variables
+    static let identifier = "bookTableViewCell"
+    static let imageViewHeight: CGFloat = 80.0
+    private let imageViewWidth: CGFloat = 60.0
+
+    // MARK: - UIViews
+    private lazy var fotoImageView: WebImageView = {
+        let imageView = WebImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .gray
+
+        return imageView
+    }()
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 17.0)
+        label.numberOfLines = 3
+        label.textAlignment = .left
+
+        return label
+    }()
+    private lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17.0)
+        label.textAlignment = .left
+
+        return label
+    }()
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(subTitleLabel)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+
+        return stackView
+    }()
+
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        createConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Functions
+    override func prepareForReuse() {
+        fotoImageView.setImage(from: nil)
+        titleLabel.text = nil
+        subTitleLabel.text = nil
+    }
+
+    func setupCell(doc: Doc, image: String?) {
+        fotoImageView.setImage(from: image)
+        titleLabel.text = doc.title
+        if let year = doc.firstPublishYear {
+            subTitleLabel.text = String(year)
+        }
+    }
+
+    private func addSubviews() {
+        contentView.addSubview(fotoImageView)
+        contentView.addSubview(stackView)
+    }
+
+    private func createConstraints() {
+        var constraints = [NSLayoutConstraint]()
+        let space: CGFloat = 5.0
+
+        constraints.append(fotoImageView.heightAnchor.constraint(equalToConstant: BookTableViewCell.imageViewHeight))
+        constraints.append(fotoImageView.widthAnchor.constraint(equalToConstant: imageViewWidth))
+        constraints.append(fotoImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor))
+        constraints.append(fotoImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: space))
+
+        constraints.append(stackView.leftAnchor.constraint(equalTo: fotoImageView.rightAnchor, constant: space))
+        constraints.append(stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: space))
+        constraints.append(stackView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -space))
+        constraints.append(stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -space))
+
+        NSLayoutConstraint.activate(constraints)
+    }
+}
