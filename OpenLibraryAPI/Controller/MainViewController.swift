6082641be151813d8d3efab3c1387560463e01c1
Mikhail <imisha.volkov@gmail.com>
2023-04-19 09:43:56 +0300

    initial first setup

--- OpenLibraryAPI/Controller/MainViewController.swift
+++ OpenLibraryAPI/Controller/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  OpenLibraryAPI
 //
 //  Created by Misha Volkov on 19.04.23.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
     }
 
 
